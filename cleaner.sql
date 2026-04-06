a515325c0c00314b879005b031244940643ad7cc
polania023 <juanpolaniareyes8@gmail.com>
2026-04-06 22:36:54 +0000

    ACTUALIZACION DE LAS CONSULTAS

--- cleaner.sql
+++ cleaner.sql
@@ -956,3 +956,25 @@ where upper(cl.ciudad) = 'Madrid'
 and em.codigo_empleado = 30
 or em.codigo_empleado = 11;
 select codigo from empleado;
+
+select '1' codigo, 'bruno diaz' empleado from dual
+union
+select '2' codigo, 'clack kent' empleado from dual;
+
+select sum(tabla2.total_registros) suma_registros,
+group_concat(tabla2.ciudad order by tabla2.ciudad separator ';') lista_ciudad
+from(select tabla.ciudad, cast(tabla.total_registros as signed) total_registros
+from (select cl.ciudad , count(*) total_registros /*cl.ciudad, em.codigo_empleado*/
+from cliente cl, empleado em
+where em.codigo_empleado = cl.codigo_empleado_rep_ventas
+and em.codigo_empleado in (11,30)
+group by cl.ciudad) tabla
+union
+select ('barcelona') ciudad, (10) total_registros from dual) tabla2;
+
+select cl.ciudad, 
+lpad(count(*),8,'0') total_registros, rpad(count(*),8,'0') total_registros
+from cliente cl, empleado em 
+where em.codigo_empleado = cl.codigo_empleado_rep_ventas
+and em.codigo_empleado in (11,30)
+group by cl.ciudad
